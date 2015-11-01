b1bbc4d74e988d59b06b8a6f895ae02d56d8306b
Richard Bowden <rb@rdbg.co.uk>
2015-11-01 17:50:28 +0000

    added basic logging handler

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 #websrvtest changelog
 
+##v0.0.2 - in progress
+* added basic request logging handler
+
 ##v0.0.1
 basic webserver, hardcoded to listen on port 8484
--- main.go
+++ main.go
@@ -4,13 +4,38 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
+
+	"log"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	h, _ := os.Hostname()
-	fmt.Fprintf(w, "Hi there, I'm served from %s!", h)
+//dont normally like globals, this will never change during the life time of the process
+var hostName string
+
+func init() {
+	var err error
+	hostName, err = os.Hostname()
+	if err != nil {
+		log.Fatal("cannot get host name of current server")
+	}
 }
+
+func logRequest(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		startReqTime := time.Now()
+		f(w, r)
+		finReqTime := time.Now()
+		log.Printf("[%s] %q %v\n", r.Method, r.URL.String(), finReqTime.Sub(startReqTime))
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I'm served from %s!", hostName)
+}
+
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8484", nil)
+	log.SetOutput(os.Stdout)
+	log.Printf("server starting on host %v", hostName)
+	http.HandleFunc("/", logRequest(handler))
+	log.Fatal(http.ListenAndServe(":8484", nil))
 }
